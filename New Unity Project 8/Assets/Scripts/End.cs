93ca785579402f70274e5a51ce5f33cddaa7486b
tony <tonydelacruzjr@gmail.com>
2017-05-10 18:13:27 -0700

    add the script
    the script that allows the scenes to change

---
+++ New Unity Project 8/Assets/Scripts/End.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour 
+{
+	GameObject flag;
+
+	// Use this for initialization
+	void Start ()
+	{
+		flag = transform.Find ("Triangle").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+			StartCoroutine (LoadNextLevel());
+	}
+
+	IEnumerator LoadNextLevel()
+	{
+		flag.GetComponent<SpriteRenderer> ().color = new Color (0, .6f, 0);
+		yield return new WaitForSeconds (2);
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex +1);
+	}
+}
